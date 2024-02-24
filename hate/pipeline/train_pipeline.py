6b43dea3d2ff8952252ce2b88855c8237347e175
soumalla <soumallatarafder@gmail.com>
2024-02-24 23:49:28 +0530

    Model trainer component everything completed

--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -3,7 +3,7 @@ from hate.logger import logging
 from hate.exception import CustomException
 from hate.components.data_ingestion import DataIngestion
 from hate.components.data_transforamation import DataTransformation
-#from hate.components.model_trainer import ModelTrainer
+from hate.components.model_trainer import ModelTrainer
 
 from hate.entity.config_entity import *
 from hate.entity.artifact_entity import *
@@ -14,7 +14,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_transformation_config = DataTransformationConfig()
-        
+        self.model_trainer_config = ModelTrainerConfig()
     
 
     def start_data_ingestion(self) -> DataIngestionArtifacts:
@@ -52,6 +52,21 @@ class TrainPipeline:
 
 
 
+    def start_model_trainer(self, data_transformation_artifacts: DataTransformationArtifacts) -> ModelTrainerArtifacts:
+        logging.info(
+            "Entered the start_model_trainer method of TrainPipeline class"
+        )
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifacts=data_transformation_artifacts,
+                                        model_trainer_config=self.model_trainer_config
+                                        )
+            model_trainer_artifacts = model_trainer.initiate_model_trainer()
+            logging.info("Exited the start_model_trainer method of TrainPipeline class")
+            return model_trainer_artifacts
+
+        except Exception as e:
+            raise CustomException(e, sys) 
+
 
 
     def run_pipeline(self):
@@ -63,7 +78,14 @@ class TrainPipeline:
                 data_ingestion_artifacts=data_ingestion_artifacts
             )
             
+            model_trainer_artifacts = self.start_model_trainer(
+                data_transformation_artifacts=data_transformation_artifacts
+            )
+            
             logging.info("Exited the run_pipeline method of TrainPipeline class") 
 
         except Exception as e:
             raise CustomException(e, sys) from e
+        
+
+    
