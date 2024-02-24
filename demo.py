133856d82e595cd56b219f6a96a6fce2a9284ab0
soumalla <soumallatarafder@gmail.com>
2024-02-24 12:53:42 +0530

    GCloud Setup And functions created and demo.py file will demontrate that with code .

--- demo.py
+++ demo.py
@@ -1,11 +1,16 @@
 from hate.logger import logging
 from hate.exception import CustomException
 import sys
+from hate.configuration.gcloud_syncer import GCloudSync
+
+# logging.info("logging tested")
+
+# try:
+#     a = 5/0;
+# except Exception as e:
+#     raise CustomException(e,sys)
+
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("myCreatedBucketName","dataZipFolderName","DataDoenloadFolderLocation")
 
 
-logging.info("logging tested")
-
-try:
-    a = 5/0;
-except Exception as e:
-    raise CustomException(e,sys)
